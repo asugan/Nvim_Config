7609d753654fed2b5db4d355d63011ad6a5e98a9
Çağatay Erkin Eren <67156394+asugan@users.noreply.github.com>
2025-07-28 17:15:23 +0300

    lazy integration

--- init.lua
+++ init.lua
@@ -21,6 +21,10 @@ vim.opt.scrolloff = 10             -- Cursor etrafında minimum satır sayısı
 vim.opt.sidescrolloff = 8          -- Cursor etrafında minimum sütun sayısı
 vim.opt.guicursor = "i-ci-ve:block" -- Insertte Block Cursor
 
+-- Syntax highlighting'i etkinleştir
+vim.cmd('syntax on')
+vim.cmd('filetype plugin indent on')
+
 -- Clipboard sistem panosunu kullan
 vim.opt.clipboard = 'unnamedplus'
 
@@ -36,6 +40,179 @@ vim.opt.swapfile = false
 -- Temel keybindler
 vim.g.mapleader = ' '              -- Leader key'i space yap
 vim.g.maplocalleader = ' '
+
+-- ===============================================
+-- PAKET YÖNETİCİSİ: Lazy.nvim
+-- ===============================================
+
+-- Lazy.nvim'i bootstrap et
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin'leri yükle
+require("lazy").setup({
+  -- ===============================================
+  -- TREESITTER - Modern Syntax Highlighting
+  -- ===============================================
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- Yüklenecek diller
+        ensure_installed = {
+          "python",
+          "javascript",
+          "typescript",
+          "tsx",
+          "html",
+          "css",
+          "json",
+          "lua",
+          "bash",
+          "markdown",
+          "yaml",
+          "toml",
+          "vim",
+          "vimdoc",
+          "go",
+          "rust",
+          "c",
+          "cpp",
+        },
+
+        -- Otomatik yükleme
+        auto_install = true,
+
+        -- Syntax highlighting
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+
+        -- Indentation
+        indent = {
+          enable = true,
+        },
+
+        -- Incremental selection
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      })
+    end,
+  },
+
+  -- ===============================================
+  -- TELESCOPE - Fuzzy Finder
+  -- ===============================================
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable "make" == 1
+        end,
+      },
+    },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-u>"] = false,
+              ["<C-d>"] = false,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+          },
+          live_grep = {
+            theme = "dropdown",
+          },
+          buffers = {
+            theme = "dropdown",
+          },
+        },
+      })
+
+      -- FZF extension'ı yükle (eğer varsa)
+      pcall(require("telescope").load_extension, "fzf")
+    end,
+  },
+})
+
+-- ===============================================
+-- DİL SPESIFIK AYARLAR
+-- ===============================================
+
+-- Python için
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
+-- JavaScript/TypeScript için
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+-- HTML/CSS için
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "html", "css", "scss", "json" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+-- ===============================================
+-- TELESCOPE KEYBİNDLER
+-- ===============================================
+
+-- Telescope keybindings
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Dosya ara' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Metin ara' })
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = 'Buffer ara' })
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Help ara' })
+vim.keymap.set('n', '<leader>fr', require('telescope.builtin').oldfiles, { desc = 'Son dosyalar' })
+vim.keymap.set('n', '<leader>fc', require('telescope.builtin').commands, { desc = 'Komutlar' })
+vim.keymap.set('n', '<leader>fk', require('telescope.builtin').keymaps, { desc = 'Keymaps' })
+
+-- ===============================================
+-- ESKİ KEYBİNDLER (değişiklik yok)
+-- ===============================================
 
 -- Typo fix'leri
 vim.cmd('command! Q q')
